b0ab5950e5f22977b66c8f25e91363260592e59d
PoolDuck <122972382+PoolDuck@users.noreply.github.com>
2023-05-11 02:01:18 +0100

    changes images directory

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -11,7 +11,7 @@ export const projects = [
   {
     title: 'E-Commerce',
     description:"While building it you're going to learn many advanced React & JavaScript topics, as well as how to use Stripe for card transactions. On top of that, at the end of the video, you will have this unique and complex webshop app that you will be able to add to your portfolio. And trust me, e-commerce applications are impressive.",
-    image: '/images/2.png',
+    image: '${process.env.PUBLIC_URL}/images/2.png',
     tags: ['React', 'JavaScript'],
     source: 'https://google.com',
     visit: 'https://google.com',
@@ -20,7 +20,7 @@ export const projects = [
   {
     title: 'WebRTC App',
     description: "This is a code repository for the corresponding YouTube video. In this tutorial, we're going to build and deploy a React Video Chat Application using WebRTC.",
-    image: '/images/3.jpg',
+    image: '${process.env.PUBLIC_URL}/images/3.png',
     tags: ['React', 'WebRTC'],
     source: 'https://google.com',
     visit: 'https://google.com',
@@ -29,7 +29,7 @@ export const projects = [
   {
     title: 'Unichat',
     description: "This is a code repository for the corresponding video tutorial. In this video, we will create a full Realtime Chat Application",
-    image: '/images/4.jpg',
+    image: '${process.env.PUBLIC_URL}/images/4.png',
     tags: ['React', 'ChatEngine', 'Firebase'],
     source: 'https://google.com',
     visit: 'https://google.com',
